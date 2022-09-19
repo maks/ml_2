5b5e0ccc47d05e635128a9b547e3e7b2534c00c3
Maksim Lin <maks@manichord.com>
2022-09-19 20:50:59 +1000

    cleanup unused code

--- lib/ml_2.dart
+++ lib/ml_2.dart
@@ -6,29 +6,6 @@ import 'package:midi/midi.dart';
 import 'package:collection/collection.dart';
 import 'package:dart_fire_midi/dart_fire_midi.dart' as fire;
 
-void testRun() async {
-  print("cwd: ${Directory.current}");
-  final sunvox = LibSunvox("./sunvox.so");
-  final v = sunvox.versionString();
-  print('sunvox lib version: $v');
-
-  const filename = "song01.sunvox";
-  await sunvox.load(filename);
-  // or as data using Dart's file ops
-  // final data = File(filename).readAsBytesSync();
-
-  sunvox.volume = 256;
-
-  sunvox.play();
-  print("playing:$filename ...");
-
-  await Future<void>.delayed(Duration(seconds: 5));
-
-  sunvox.stop();
-
-  sunvox.shutDown();
-}
-
 class ML2 {
   late final LibSunvox _sunvox;
 
