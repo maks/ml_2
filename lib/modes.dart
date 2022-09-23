68ef62f245a9866f68c15b4cd4f60693b1f6ac4a
Maksim Lin <maks@manichord.com>
2022-09-23 21:34:50 +1000

    add devicemode interface

---
+++ lib/modes.dart
@@ -0,0 +1,96 @@
+abstract class DeviceMode {
+  void onButton();
+  void onPad();
+  void onDial();
+
+  void onUpdate();
+}
+
+class StepMode implements DeviceMode {
+  @override
+  void onButton() {
+    // TODO: implement onButton
+  }
+
+  @override
+  void onDial() {
+    // TODO: implement onDial
+  }
+
+  @override
+  void onPad() {
+    // TODO: implement onPad
+  }
+
+  @override
+  void onUpdate() {
+    // TODO: implement onUpdate
+  }
+}
+
+class NoteMode implements DeviceMode {
+  @override
+  void onButton() {
+    // TODO: implement onButton
+  }
+
+  @override
+  void onDial() {
+    // TODO: implement onDial
+  }
+
+  @override
+  void onPad() {
+    // TODO: implement onPad
+  }
+
+  @override
+  void onUpdate() {
+    // TODO: implement onUpdate
+  }
+}
+
+/// uses the "drum mdoe" button
+class ModuleMode implements DeviceMode {
+  @override
+  void onButton() {
+    // TODO: implement onButton
+  }
+
+  @override
+  void onDial() {
+    // TODO: implement onDial
+  }
+
+  @override
+  void onPad() {
+    // TODO: implement onPad
+  }
+
+  @override
+  void onUpdate() {
+    // TODO: implement onUpdate
+  }
+}
+
+class PerformMode implements DeviceMode {
+  @override
+  void onButton() {
+    // TODO: implement onButton
+  }
+
+  @override
+  void onDial() {
+    // TODO: implement onDial
+  }
+
+  @override
+  void onPad() {
+    // TODO: implement onPad
+  }
+
+  @override
+  void onUpdate() {
+    // TODO: implement onUpdate
+  }
+}
