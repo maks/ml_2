a3b2b6fe81be8c58362cc0010de7d841eb87a40a
Maksim Lin <maks@manichord.com>
2022-10-29 20:49:30 +1100

    show controller names per page on Oled

--- lib/modes/module_mode.dart
+++ lib/modes/module_mode.dart
@@ -1,8 +1,12 @@
-import 'package:bonsai/bonsai.dart';
+import 'dart:math' as math;
+
 import 'package:dart_fire_midi/dart_fire_midi.dart';
 import 'package:dart_sunvox/dart_sunvox.dart';
+import 'package:ml_2/extensions.dart';
+import 'package:ml_2/pads.dart';
 import 'package:ml_2/widgets/widget.dart';
 
+import 'package:bonsai/bonsai.dart';
 import '../modifiers.dart';
 import '../widgets/chromatic_keyboard.dart';
 import '../widgets/module_list.dart';
@@ -46,10 +50,16 @@ class ModuleMode implements DeviceMode {
         _toggleBrowserMode();
       }
       if (event.type == ButtonType.PatternUp) {
-        _controllerPage = _controllerPage + 1;
+        final controllersCount = _context.currentModule?.controllers.length ?? 0;
+        log("cntrl count: $controllersCount");
+        _controllerPage = math.min((controllersCount / 3).floor(), _controllerPage + 1);
       }
       if (event.type == ButtonType.PatternDown) {
-        _controllerPage = _controllerPage - 1;
+        _controllerPage = math.max(0, _controllerPage - 1);
+      }
+    } else {
+      if (event.type == ButtonType.PatternUp || event.type == ButtonType.PatternDown) {
+        _showControllersPageOnOled();
       }
     }
     log("controller page: $_controllerPage");
@@ -105,13 +115,18 @@ class ModuleMode implements DeviceMode {
         }
       }
     }
-    _context.screen.drawContent([controller.name, "${controller.displayValue ?? controller.value}"], large: true);
+    //_context.screen.drawContent([controller.name, "${controller.displayValue ?? controller.value}"], large: true);
+    _showControllersPageOnOled();
   }
 
   @override
   void onPad(PadEvent event, Modifiers mods) {
     for (final child in _children) {
       child.onPad(event, mods);
+    }
+    // TODO: for now workaround for detecting module pad change
+    if (padIndexFrom(event.column, event.row) < 31) {
+      _controllerPage = 0; //reset controller page
     }
   }
 
@@ -156,6 +171,24 @@ class ModuleMode implements DeviceMode {
     paint();
   }
 
+  void _showControllersPageOnOled() {
+    final controllerNames = <String>[];
+    final controllers = _context.currentModule?.controllers;
+    if (controllers == null) {
+      log("no current controller");
+      return;
+    }
+    for (int i in [0, 1, 2]) {
+      final controllerIndex = i + (_controllerPage * 3);
+      if (controllerIndex >= controllers.length) {
+        continue;
+      }
+      final controller = controllers[controllerIndex];
+      controllerNames.add("${_controllerAbrv(controller.name)} ${controller.displayValue.toString().truncate(4)}");
+    }
+    _context.screen.drawContent(controllerNames, large: true);
+  }
+
   @override
   void onFocus() {
     for (final child in _children) {
@@ -168,3 +201,95 @@ class AllModulesListProvider implements ListItemProvider {
   @override
   String? itemName(int index) => index < allModuleTypes.length ? allModuleTypes.elementAt(index) : null;
 }
+
+String _controllerAbrv(String name) {
+  if (controllerAbbreviations[name] != null) {
+    return controllerAbbreviations[name]!;
+  } else {
+    print("missing abrv:$name");
+    return name.truncate(3);
+  }
+}
+
+const controllerAbbreviations = {
+  "Volume": "VOL ",
+  "Attack": "ATK ",
+  "Release": "REL ",
+  "Sustain": "SUS ",
+  "Panning": "PAN ",
+  "Waveform": "WAV ",
+  "Exponential envelope": "ENV ",
+  "Duty cycle": "DUTY",
+  "Osc2": "OSX2",
+  "Filter": "FLTR",
+  "F.freq": "FLFR",
+  "F.resonance": "FLRE",
+  "F.exponential freq": "FLEF",
+  "F.attack": "FLAK",
+  "F.release": "FLRL",
+  "F.envelope": "FLEN",
+  "Polyphony": "POLY",
+  "Wet": "WET ",
+  "Dry": "DRY ",
+  "Feedback": "FDBK",
+  "Damp": "DAMP",
+  "Type": "TYPE",
+  "BPM (Beats Per Minute)": "BPM ",
+  "TPL (Ticks Per Line)": "TPL  ",
+  "Input module": "INMD",
+  "Play patterns": "PLPT",
+  "Mode": "MODE",
+  "H.type": "HTYP",
+  "H.freq": "HFRQ",
+  "H.volume": "HVOL",
+  "H.width": "HWDT",
+  "Spectrum resolution": "SRES",
+  "Harmonic": "HARM",
+  "Room Size": "RMSZ",
+  "Tone": "TONE",
+  "Noise": "NOIS",
+  "Osc2 volume": "OC2V",
+  "Osc2 mode": "OC2M",
+  "Osc2 phase": "OC2P",
+  "Side-chain input": "SCIN",
+  "Threshold": "THRS",
+  "Slope": "SLOP",
+  "Channels": "CHAN",
+  "Set phase": "SETP",
+  "Frequency unit": "FRQU",
+  "Exponential amplitude": "EXPA",
+  "Amplitude": "AMPL",
+  "Freq": "FREQ",
+  "Delay": "DELY",
+  "Right channel offset": "RCHN",
+  "Delay unit": "DLYU",
+  "Input -> Operator #": "INOP",
+  "Input -> Custom waveform": "INCW",
+  "ADSR smooth transitions": "ADSR",
+  "Noise filter (32768 - OFF)": "NOFL",
+  "1 Volume": "1VOL",
+  "2 Volume": "2VOL",
+  "3 Volume": "3VOL",
+  "4 Volume": "4VOL",
+  "5 Volume": "5VOL",
+  "1 Attack": "1ATK",
+  "2 Attack": "2ATK",
+  "3 Attack": "3ATK",
+  "4 Attack": "4ATK",
+  "5 Attack": "5ATK",
+  "1 Decay": "1DCY",
+  "2 Decay": "2DCY",
+  "3 Decay": "3DCY",
+  "4 Decay": "4DCY",
+  "5 Decay": "5DCY",
+  "1 Sustain level": "1SUS",
+  "2 Sustain level": "2SUS",
+  "3 Sustain level": "3SUS",
+  "4 Sustain level": "4SUS",
+  "5 Sustain level": "5SUS",
+  "1 Release": "1REL",
+  "2 Release": "2REL",
+  "3 Release": "3REL",
+  "4 Release": "4REL",
+  "5 Release": "5REL",
+};
