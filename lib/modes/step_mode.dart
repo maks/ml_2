984ca0347a8f2ce6437f9dc599190a67f89ebe9a
Maksim Lin <maks@manichord.com>
2023-02-14 16:53:06 +1100

    display pattern note on pad press

--- lib/modes/step_mode.dart
+++ lib/modes/step_mode.dart
@@ -1,4 +1,5 @@
 import 'package:dart_fire_midi/dart_fire_midi.dart';
+import 'package:ml_2/extensions.dart';
 import 'package:ml_2/sequencer/pattern.dart';
 
 import '../modifiers.dart';
@@ -8,11 +9,23 @@ import 'mode.dart';
 class StepMode implements DeviceMode {
   final WidgetContext _context;
 
+  String _pressedStepNote = "";
+  int _page = 0;
+  int _patternLength = 0;
+
   StepMode(this._context);
   
   @override
   void onButton(ButtonEvent event, Modifiers mods) {
-    // TODO: implement onButton
+    if (event.direction == ButtonDirection.Down) {
+      if (event.type == ButtonType.GridLeft) {
+        _page = _page.decrement();
+      }
+      if (event.type == ButtonType.GridRight) {
+        _page = _page.increment((_patternLength % 16) + 1);
+      }
+      print("page: $_page");
+    }
   }
 
   @override
@@ -23,25 +36,40 @@ class StepMode implements DeviceMode {
   @override
   void onPad(PadEvent event, Modifiers mods) {
     if (event.direction == ButtonDirection.Up) {
+      _pressedStepNote = "";
       return;
     }
+    final patterns = _getPatterns();
+    final track = event.row;
+    final step = event.column + (16 * _page);
+    //TODO: use selected pattern
+    final note = patterns.first.tracks[track].steps[step].noteAsString;
+    _patternLength = patterns.first.tracks.first.steps.length;
+    print("Track:$track Step:$step Note:$note [$_patternLength]");
+    _pressedStepNote = note;
+  }
+
+  @override
+  void paint() {
+    print("step mode paint()");
+    _context.screen.drawContent([_pressedStepNote], large: true);
+  }
+
+  @override
+  void onFocus() {}
+
+  List<SqPattern> _getPatterns() {
     final pCount = _context.sunvox.patternCount;
     final List<SqPattern> patterns = [];
     for (int i = 0; i < pCount; i++) {
       final pat = _context.sunvox.getPattern(i);
       if (pat != null) {
         patterns.add(SqPattern.fromSunvox(pat));
-      }      
+      }
     }
-    print("Pattern first note: ${patterns.first.tracks.first.steps.first.noteAsString}");    
-  }
-
-  @override
-  void paint() {
-    
-  }
-
-  @override
-  void onFocus() {
+    print("read sunvox patterns:\n $patterns");
+    //print("Pattern first note: ${patterns.first.tracks.first.steps.first.noteAsString}");
+   
+    return patterns;
   }
 }
